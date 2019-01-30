87c22f3682fd96a27d21c5cdfef67c746148c0cc
MelissieP <melissa.pistorius@gmail.com>
2019-01-30 14:09:47 +0200

    Removed data processing from these scripts

--- submission.py
+++ submission.py
@@ -1,308 +1,91 @@
-import pandas as pd
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, BatchNormalization
 from keras import utils
 from sklearn.model_selection import train_test_split
 from keras.layers import Dropout
-from keras.callbacks import LearningRateScheduler, Callback
-from sklearn.metrics import cohen_kappa_score
-from scipy import floor, power, exp
-from keras.optimizers import SGD, Adam
+import json
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings('ignore')
 
-def read_in_data():
-    train = pd.read_csv("input/train.csv")
-    test = pd.read_csv("input/test.csv")
-    breeds = pd.read_csv("input/breed_labels.csv")
-    colours = pd.read_csv("input/color_labels.csv")
-    return train, test, breeds, colours
-
-def standardise_unnamed_pets(train, test):
-    train["Name"] = train["Name"].fillna("Unnamed")
-    train["Name"] = np.where(train["Name"] == "No Name", "Unnamed", train["Name"])
-    train["Name"] = np.where(train["Name"] == "None", "Unnamed", train["Name"])
-    train["Name"] = np.where(train["Name"] == "No Name Yet", "Unnamed", train["Name"])
-
-    test["Name"] = test["Name"].fillna("Unnamed")
-    test["Name"] = np.where(test["Name"] == "No Name", "Unnamed", test["Name"])
-    test["Name"] = np.where(test["Name"] == "None", "Unnamed", test["Name"])
-    test["Name"] = np.where(test["Name"] == "No Name Yet", "Unnamed", test["Name"])
-    return train, test
-
-def create_unnamed_feature(train, test):
-    train["Unnamed"] = 0
-    train.loc[train["Name"] == "Unnamed", "Unnamed"] = 1
-
-    test["Unnamed"] = 0
-    test.loc[test["Name"] == "Unnamed", "Unnamed"] = 1
-    return train, test
-
-def drop_na_values(train, test):
-    train = train.fillna(0)
-    test = test.fillna(0)
-    return train, test
-
-def create_mixed_breed_feature(train, test):
-    train["MixedBreed"] = [1 if x > 0 else 0 for x in train["Breed2"]]
-    test["MixedBreed"] = [1 if x > 0 else 0 for x in test["Breed2"]]
-    return train, test
-
-def breed_df_to_dict(breeds):
-    breed_cols = breeds[["BreedID", "BreedName"]]
-    breed_cols.set_index("BreedID", drop=True, inplace=True)
-    breed_dict = breed_cols.to_dict()["BreedName"]
-    return breed_dict
-
-def map_dictionary_to_breed_names(breed_dict, train, test):
-    train["BreedName_1"] = train["Breed1"].map(breed_dict).fillna("Unknown")
-    train["BreedName_2"] = train["Breed2"].map(breed_dict).fillna("None")
-
-    test["BreedName_1"] = test["Breed1"].map(breed_dict).fillna("Unknown")
-    test["BreedName_2"] = test["Breed2"].map(breed_dict).fillna("None")
-
-    # Make all the cases pure bred if breed 1 is the same as breed 2
-    train.loc[train["BreedName_1"] == train["BreedName_2"], "MixedBreed"] = 0
-    test.loc[test["BreedName_1"] == test["BreedName_2"], "MixedBreed"] = 0
-
-    # But for the following cases, they are mixed:
-    train.loc[train["BreedName_1"] == "Mixed Breed", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Short Hair", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Medium Hair", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Long Hair", "MixedBreed"] = 1
-
-    test.loc[test["BreedName_1"] == "Mixed Breed", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Short Hair", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Medium Hair", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Long Hair", "MixedBreed"] = 1
-
-    return train, test
-
-def create_description_length_feature(train, test):
-    train["Description"] = train["Description"].fillna("")
-    train["Description_Character_Count"] = train["Description"].apply(lambda x: len(x))
-    train["Description_Word_Count"] = train["Description"].apply(lambda x: len(x.split()))
-
-    test["Description"] = test["Description"].fillna("")
-    test["Description_Character_Count"] = test["Description"].apply(lambda x: len(x))
-    test["Description_Word_Count"] = test["Description"].apply(lambda x: len(x.split()))
-    return train, test
-
-train, test, breeds, colours = read_in_data()
-train, test = standardise_unnamed_pets(train, test)
-train, test = create_unnamed_feature(train, test)
-train, test = create_mixed_breed_feature(train, test)
-breed_dict = breed_df_to_dict(breeds)
-train, test = map_dictionary_to_breed_names(breed_dict, train, test)
-train, test = create_description_length_feature(train, test)
-
-train, test = drop_na_values(train, test)
-
-def create_character_quantile_columns(df):
-    character_25 = df["Description_Character_Count"].quantile(q=0.25)
-    character_5 = df["Description_Character_Count"].quantile(q=0.5)
-    character_75 = df["Description_Character_Count"].quantile(q=0.75)
-
-    df["Character_25"] = 0
-    df.loc[df["Description_Character_Count"] <= character_25, "Character_25"] = 1
-
-    df["Character_5"] = 0
-    df.loc[(df["Description_Character_Count"] > character_25) & (
-            df["Description_Character_Count"] <= character_5), "Character_5"] = 1
-
-    df["Character_75"] = 0
-    df.loc[(df["Description_Character_Count"] > character_5) & (
-            df["Description_Character_Count"] <= character_75), "Character_75"] = 1
-
-    df["Character_100"] = 0
-    df.loc[(df["Description_Character_Count"] > character_75), "Character_100"] = 1
-
-    return df
-
-def create_word_quantile_columns(df):
-    word_25 = df["Description_Word_Count"].quantile(q=0.25)
-    word_5 = df["Description_Word_Count"].quantile(q=0.5)
-    word_75 = df["Description_Word_Count"].quantile(q=0.75)
-
-    df["Word_25"] = 0
-    df.loc[df["Description_Word_Count"] <= word_25, "Word_25"] = 1
-
-    df["Word_5"] = 0
-    df.loc[(df["Description_Word_Count"] > word_25) & (
-            df["Description_Word_Count"] <= word_5), "Word_5"] = 1
-
-    df["Word_75"] = 0
-    df.loc[(df["Description_Word_Count"] > word_5) & (
-            df["Description_Word_Count"] <= word_75), "Word_75"] = 1
-
-    df["Word_100"] = 0
-    df.loc[(df["Description_Word_Count"] > word_75), "Word_100"] = 1
-    return df
-
-def bucket_ages(df):
-    # Drop Ages thats = 0
-    df = df[df["Age"] > 0]
-    df["Puppy"] = 0
-    df["Adult"] = 0
-    df["Senior"] = 0
-    df.loc[(df["Age"] >= 1) & (df["Age"] < 12), "Puppy"] = 1
-    df.loc[(df["Age"] >= 12) & (df["Age"] < 96), "Adult"] = 1
-    df.loc[(df["Age"] >= 96, "Senior")] = 1
-    return df
-
-def bucket_fees(df):
-    fee_25 = df["Fee"].quantile(q=0.25)
-    fee_5 = df["Fee"].quantile(q=0.5)
-    fee_75 = df["Fee"].quantile(q=0.75)
-    df["No_Fee"] = 0
-    df["Fee_25"] = 0
-    df["Fee_5"] = 0
-    df["Fee_75"] = 0
-    df["Fee_100"] = 0
-    df.loc[df["Fee"] == 0, "No_Fee"] = 1
-    df.loc[(df["Fee"] > 0) & (
-            df["Fee"] <= fee_25), "Fee_25"] = 1
-    df.loc[(df["Fee"] > fee_25) & (
-            df["Fee"] <= fee_5), "Fee_5"] = 1
-    df.loc[(df["Fee"] > fee_5) & (
-            df["Fee"] <= fee_75), "Fee_75"] = 1
-    df.loc[(df["Fee"] > fee_75), "Fee_100"] = 1
-    return df
-
-test = create_character_quantile_columns(test)
-train = create_word_quantile_columns(train)
-test = create_word_quantile_columns(test)
-train = bucket_ages(train)
-test = bucket_ages(test)
-train = bucket_fees(train)
-test = bucket_fees(test)
+def process_data_for_model():
+    data = pd.read_csv("data.csv")
+    data = pd.get_dummies(data, columns = ["Gender", "MaturitySize",
+                                             "Vaccinated", "Dewormed", "Sterilized",
+                                             "Health", 'Quantity'])
+    y = data["AdoptionSpeed"]
 
 
-y = train["AdoptionSpeed"]
+    data = data.drop(
+        columns=["AdoptionSpeed", "PetID", "Name", "State", "RescuerID", "Description", "BreedName_1", "BreedName_2",
+                 "Fee", "Description_Character_Count", "Description_Word_Count", "Age",  'Color1', 'Color2', 'Color3', "FurLength", 'Breed1', 'Breed2'])
+
+    scaler = StandardScaler().fit(data)
+    data = scaler.transform(data)
 
 
-train = train.drop(
-    columns=["AdoptionSpeed", "PetID", "Name", "State", "RescuerID", "Description", "BreedName_1", "BreedName_2",
-            "Fee", "Description_Character_Count", "Description_Word_Count", "Age"])
+    X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.3)
 
-data = pd.get_dummies(train, columns = ["Type", "Gender", "MaturitySize", "FurLength",
-                                         "Vaccinated", "Dewormed", "Sterilized",
-                                         "Health", 'Breed1', 'Breed2', 'Color1', 'Color2', 'Color3', 'Quantity'])
+    #  Separate target variable from training set, and remove unnecessary columns
+    encoder = LabelEncoder()
+    encoder.fit(Y_train)
+    y_train = encoder.transform(Y_train)
+    y_test = encoder.transform(Y_test)
 
-X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.33, random_state=1)
+    num_classes = np.max(y_train) + 1
+    y_train = utils.to_categorical(y_train, num_classes)
+    y_test = utils.to_categorical(y_test, num_classes)
 
-#  Separate target variable from training set, and remove unnecessary columns
-encoder = LabelEncoder()
-encoder.fit(Y_train)
-y_train = encoder.transform(Y_train)
-y_test = encoder.transform(Y_test)
+    return X_train, X_test, y_train, y_test, num_classes
 
-num_classes = np.max(y_train) + 1
-y_train = utils.to_categorical(y_train, num_classes)
-y_test = utils.to_categorical(y_test, num_classes)
+def build_model(X_train, X_test, y_train, y_test, num_classes):
+    batch_size = 258
+    epochs = 500
 
-batch_size = 128
-epochs = 500
+    # Build the model
+    model = Sequential()
+    model.add(Dense(114, input_shape=(57,)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.1))
 
-# Build the model
-model = Sequential()
-model.add(Dense(388, input_shape=(388,)))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    model.add(Dense(100))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.1))
 
-model.add(Dense(350))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    model.add(Dense(50))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.1))
 
-model.add(Dense(300))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    model.add(Dense(25))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.1))
 
-model.add(Dense(250))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    model.add(Dense(num_classes))
+    model.add(BatchNormalization())
+    model.add(Activation('softmax'))
 
-model.add(Dense(200))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    model.compile(loss='binary_crossentropy',
+                  optimizer="rmsprop",
+                  metrics=['accuracy'])
 
-model.add(Dense(100))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    history = model.fit(X_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=2)
 
-model.add(Dense(50))
-model.add(BatchNormalization())
-model.add(Activation('selu'))
-#model.add(Dropout(0.2))
+    score = model.evaluate(X_test, y_test,
+                           batch_size=batch_size, verbose=1)
 
-model.add(Dense(20))
-model.add(BatchNormalization())
-#model.add(Activation('selu'))
+    print('Test accuracy:', score[1] * 100)
 
-model.add(Dense(num_classes))
-model.add(BatchNormalization())
-model.add(Activation('softmax'))
+    return score[1] * 100
 
-# drop = 0.5
-# epochs_drop = 1.0
-# initial_lrate = 0.1
-#
-# learning_rate = 0.1
-# decay_rate = learning_rate / epochs
-momentum = 0.8
-sgd = Adam(lr=0.0, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-
-def exp_decay(epoch):
-   initial_lrate = 0.01
-   k = 0.1
-   lrate = initial_lrate * exp(-k*epoch)
-   return lrate
-lrate = LearningRateScheduler(exp_decay)
-
-
-class LossHistory(Callback):
-    def on_train_begin(self, logs={}):
-        self.losses = []
-        self.lr = []
-
-    def on_epoch_end(self, batch, logs={}):
-        self.losses.append(logs.get("loss"))
-        self.lr.append(exp_decay(len(self.losses)))
-
-loss_history = LossHistory()
-lrate = LearningRateScheduler(exp_decay)
-callbacks_list = [loss_history, lrate]
-
-
-model.compile(loss='categorical_crossentropy',
-              optimizer="rmsprop",
-              metrics=['categorical_accuracy'])
-
-history = model.fit(X_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=2,
-                    validation_split = 0.3,
-                    callbacks=callbacks_list)
-
-score = model.evaluate(X_test, y_test,
-                       batch_size=batch_size, verbose=1)
-
-print('Test accuracy:', score[1] * 100)
-
-# pred = model.predict(X_test, batch_size=batch_size, verbose=1)
-
-# kappa = cohen_kappa_score(y_test, pred)
-#
-# print(kappa)
+X_train, X_test, y_train, y_test, num_classes = process_data_for_model()
+test_accuracy = build_model(X_train, X_test, y_train, y_test, num_classes)
--- xgboost-model.py
+++ xgboost-model.py
@@ -1,223 +1,45 @@
 import warnings
 warnings.filterwarnings('ignore')
-import numpy as np
 import pandas as pd
-from datetime import datetime
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import StandardScaler
 
-def read_in_data():
-    train = pd.read_csv("input/train.csv")
-    test = pd.read_csv("input/test.csv")
-    breeds = pd.read_csv("input/breed_labels.csv")
-    colours = pd.read_csv("input/color_labels.csv")
-    return train, test, breeds, colours
-
-def standardise_unnamed_pets(train, test):
-    train["Name"] = train["Name"].fillna("Unnamed")
-    train["Name"] = np.where(train["Name"] == "No Name", "Unnamed", train["Name"])
-    train["Name"] = np.where(train["Name"] == "None", "Unnamed", train["Name"])
-    train["Name"] = np.where(train["Name"] == "No Name Yet", "Unnamed", train["Name"])
-
-    test["Name"] = test["Name"].fillna("Unnamed")
-    test["Name"] = np.where(test["Name"] == "No Name", "Unnamed", test["Name"])
-    test["Name"] = np.where(test["Name"] == "None", "Unnamed", test["Name"])
-    test["Name"] = np.where(test["Name"] == "No Name Yet", "Unnamed", test["Name"])
-    return train, test
-
-def create_unnamed_feature(train, test):
-    train["Unnamed"] = 0
-    train.loc[train["Name"] == "Unnamed", "Unnamed"] = 1
-
-    test["Unnamed"] = 0
-    test.loc[test["Name"] == "Unnamed", "Unnamed"] = 1
-    return train, test
-
-def drop_na_values(train, test):
-    train = train.fillna(0)
-    test = test.fillna(0)
-    return train, test
-
-def create_mixed_breed_feature(train, test):
-    train["MixedBreed"] = [1 if x > 0 else 0 for x in train["Breed2"]]
-    test["MixedBreed"] = [1 if x > 0 else 0 for x in test["Breed2"]]
-    return train, test
-
-def breed_df_to_dict(breeds):
-    breed_cols = breeds[["BreedID", "BreedName"]]
-    breed_cols.set_index("BreedID", drop=True, inplace=True)
-    breed_dict = breed_cols.to_dict()["BreedName"]
-    return breed_dict
-
-def map_dictionary_to_breed_names(breed_dict, train, test):
-    train["BreedName_1"] = train["Breed1"].map(breed_dict).fillna("Unknown")
-    train["BreedName_2"] = train["Breed2"].map(breed_dict).fillna("None")
-
-    test["BreedName_1"] = test["Breed1"].map(breed_dict).fillna("Unknown")
-    test["BreedName_2"] = test["Breed2"].map(breed_dict).fillna("None")
-
-    # Make all the cases pure bred if breed 1 is the same as breed 2
-    train.loc[train["BreedName_1"] == train["BreedName_2"], "MixedBreed"] = 0
-    test.loc[test["BreedName_1"] == test["BreedName_2"], "MixedBreed"] = 0
-
-    # But for the following cases, they are mixed:
-    train.loc[train["BreedName_1"] == "Mixed Breed", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Short Hair", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Medium Hair", "MixedBreed"] = 1
-    train.loc[train["BreedName_1"] == "Domestic Long Hair", "MixedBreed"] = 1
-
-    test.loc[test["BreedName_1"] == "Mixed Breed", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Short Hair", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Medium Hair", "MixedBreed"] = 1
-    test.loc[test["BreedName_1"] == "Domestic Long Hair", "MixedBreed"] = 1
-
-    return train, test
-
-def create_description_length_feature(train, test):
-    train["Description"] = train["Description"].fillna("")
-    train["Description_Character_Count"] = train["Description"].apply(lambda x: len(x))
-    train["Description_Word_Count"] = train["Description"].apply(lambda x: len(x.split()))
-
-    test["Description"] = test["Description"].fillna("")
-    test["Description_Character_Count"] = test["Description"].apply(lambda x: len(x))
-    test["Description_Word_Count"] = test["Description"].apply(lambda x: len(x.split()))
-    return train, test
-
-train, test, breeds, colours = read_in_data()
-train, test = standardise_unnamed_pets(train, test)
-train, test = create_unnamed_feature(train, test)
-train, test = create_mixed_breed_feature(train, test)
-breed_dict = breed_df_to_dict(breeds)
-train, test = map_dictionary_to_breed_names(breed_dict, train, test)
-train, test = create_description_length_feature(train, test)
-
-train, test = drop_na_values(train, test)
-
-def create_character_quantile_columns(df):
-    character_25 = df["Description_Character_Count"].quantile(q=0.25)
-    character_5 = df["Description_Character_Count"].quantile(q=0.5)
-    character_75 = df["Description_Character_Count"].quantile(q=0.75)
-
-    df["Character_25"] = 0
-    df.loc[df["Description_Character_Count"] <= character_25, "Character_25"] = 1
-
-    df["Character_5"] = 0
-    df.loc[(df["Description_Character_Count"] > character_25) & (
-            df["Description_Character_Count"] <= character_5), "Character_5"] = 1
-
-    df["Character_75"] = 0
-    df.loc[(df["Description_Character_Count"] > character_5) & (
-            df["Description_Character_Count"] <= character_75), "Character_75"] = 1
-
-    df["Character_100"] = 0
-    df.loc[(df["Description_Character_Count"] > character_75), "Character_100"] = 1
-
-    return df
-
-def create_word_quantile_columns(df):
-    word_25 = df["Description_Word_Count"].quantile(q=0.25)
-    word_5 = df["Description_Word_Count"].quantile(q=0.5)
-    word_75 = df["Description_Word_Count"].quantile(q=0.75)
-
-    df["Word_25"] = 0
-    df.loc[df["Description_Word_Count"] <= word_25, "Word_25"] = 1
-
-    df["Word_5"] = 0
-    df.loc[(df["Description_Word_Count"] > word_25) & (
-            df["Description_Word_Count"] <= word_5), "Word_5"] = 1
-
-    df["Word_75"] = 0
-    df.loc[(df["Description_Word_Count"] > word_5) & (
-            df["Description_Word_Count"] <= word_75), "Word_75"] = 1
-
-    df["Word_100"] = 0
-    df.loc[(df["Description_Word_Count"] > word_75), "Word_100"] = 1
-    return df
-
-def bucket_ages(df):
-    # Drop Ages thats = 0
-    df = df[df["Age"] > 0]
-    df["Puppy"] = 0
-    df["Adult"] = 0
-    df["Senior"] = 0
-    df.loc[(df["Age"] >= 1) & (df["Age"] < 12), "Puppy"] = 1
-    df.loc[(df["Age"] >= 12) & (df["Age"] < 96), "Adult"] = 1
-    df.loc[(df["Age"] >= 96, "Senior")] = 1
-    return df
-
-def bucket_fees(df):
-    fee_25 = df["Fee"].quantile(q=0.25)
-    fee_5 = df["Fee"].quantile(q=0.5)
-    fee_75 = df["Fee"].quantile(q=0.75)
-    df["No_Fee"] = 0
-    df["Fee_25"] = 0
-    df["Fee_5"] = 0
-    df["Fee_75"] = 0
-    df["Fee_100"] = 0
-    df.loc[df["Fee"] == 0, "No_Fee"] = 1
-    df.loc[(df["Fee"] > 0) & (
-            df["Fee"] <= fee_25), "Fee_25"] = 1
-    df.loc[(df["Fee"] > fee_25) & (
-            df["Fee"] <= fee_5), "Fee_5"] = 1
-    df.loc[(df["Fee"] > fee_5) & (
-            df["Fee"] <= fee_75), "Fee_75"] = 1
-    df.loc[(df["Fee"] > fee_75), "Fee_100"] = 1
-    return df
-
-test = create_character_quantile_columns(test)
-train = create_word_quantile_columns(train)
-test = create_word_quantile_columns(test)
-train = bucket_ages(train)
-test = bucket_ages(test)
-train = bucket_fees(train)
-test = bucket_fees(test)
+def process_data_for_model():
+    data = pd.read_csv("data.csv")
+    data = pd.get_dummies(data, columns = ["Gender", "MaturitySize",
+                                             "Vaccinated", "Dewormed", "Sterilized",
+                                             "Health", 'Quantity','Color1', 'Color2', 'Color3', "FurLength", 'Breed1', 'Breed2'])
+    y = data["AdoptionSpeed"]
 
 
-y = train["AdoptionSpeed"]
+    data = data.drop(
+        columns=["AdoptionSpeed", "PetID", "Name", "State", "RescuerID", "Description", "BreedName_1", "BreedName_2",
+                 "Fee", "Description_Character_Count", "Description_Word_Count", "Age"])
+
+    scaler = StandardScaler().fit(data)
+    data = scaler.transform(data)
 
 
-train = train.drop(
-    columns=["AdoptionSpeed", "PetID", "Name", "State", "RescuerID", "Description", "BreedName_1", "BreedName_2",
-            "Fee", "Description_Character_Count", "Description_Word_Count", "Age"])
+    X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.2, random_state=1)
 
-data = pd.get_dummies(train, columns = ["Type", "Gender", "MaturitySize", "FurLength",
-                                         "Vaccinated", "Dewormed", "Sterilized",
-                                         "Health", 'Breed1', 'Breed2', 'Color1', 'Color2', 'Color3', 'Quantity'])
+    return X_train, X_test, Y_train, Y_test
 
-X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.2, random_state=1)
+def train_and_test_model(X_train, X_test, Y_train, Y_test):
+    xgb = XGBClassifier(learning_rate=0.02, objective='multi:softmax',
+                    silent=True, nthread=1, num_class = 5,
+                    max_depth = 20, gamma = 0.5, colsample_bytree = 1.0, min_child_weight = 5,
+                    subsample = 0.8, n_estimators = 300)
 
-# A parameter grid for XGBoost
-params = {
-        'min_child_weight': [1, 5, 10],
-        'gamma': [0.5, 1, 1.5, 2, 5],
-        'subsample': [0.6, 0.8, 1.0],
-        'colsample_bytree': [0.6, 0.8, 1.0],
-        'max_depth': [5, 10, 15],
-        'n_estimators':[50, 200, 500]
-        }
+    model = xgb.fit(X_train, Y_train)
 
-xgb = XGBClassifier(learning_rate=0.02, objective='multi:softmax',
-                    silent=True, nthread=1, num_class = 5)
+    pred = model.predict(X_test)
 
-folds = 3
-param_comb = 5
+    acc = accuracy_score(Y_test, pred)
 
-skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 1001)
+    return acc
 
-random_search = RandomizedSearchCV(xgb, param_distributions=params, n_iter=param_comb, scoring=['f1_macro','precision_macro'], cv=skf.split(X_train,Y_train),
-                                   refit='f1_macro', verbose=3, random_state=1001 )
-
-random_search.fit(X_train, Y_train)
-
-print('\n All results:')
-print(random_search.cv_results_)
-print('\n Best estimator:')
-print(random_search.best_estimator_)
-print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
-print(random_search.best_score_ * 2 - 1)
-print('\n Best hyperparameters:')
-print(random_search.best_params_)
+X_train, X_test, Y_train, Y_test = process_data_for_model()
+acc = train_and_test_model(X_train, X_test, Y_train, Y_test)
+print(acc)
